5f50ab5f9cae0bbac8840fd1c7329ad460dd0075
NotIsmail <mohammedismail61405@gmail.com>
2024-01-25 22:02:58 +0530

    Python learnt 1

---
+++ Hello.py
@@ -0,0 +1,6 @@
+print("Hello world!!")
+Author_Name="Ismail"
+Author_Age=18
+print(f"Say Hello to {Author_Name} He is {Author_Age}")
+Author_Likings,Author_Movies,Author_Games="Gaming","La la land","GTA 5"
+print(f"Your Author {Author_Name} Likes {Author_Likings} {Author_Movies} {Author_Games}")
