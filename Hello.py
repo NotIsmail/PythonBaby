72b9af1f6e98432de8267f1fb403073ab728642d
NotIsmail <mohammedismail61405@gmail.com>
2024-01-26 20:47:57 +0530

    Here a program revision for taking input

--- Hello.py
+++ Hello.py
@@ -1,6 +1,11 @@
 print("Hello world!!")
-Author_Name="Ismail"
-Author_Age=18
+Author_Name = "Ismail"
+Author_Age = 18
 print(f"Say Hello to {Author_Name} He is {Author_Age}")
-Author_Likings,Author_Movies,Author_Games="Gaming","La la land","GTA 5"
-print(f"Your Author {Author_Name} Likes {Author_Likings} {Author_Movies} {Author_Games}")
+Author_Likings, Author_Movies, Author_Games = "Gaming", "La la land", "GTA 5"
+print(
+    f"Your Author {Author_Name} Likes {Author_Likings} {Author_Movies} {Author_Games}"
+)
+User_Name = input("Enter your Name:")
+User_Age = int(input("Enter your Age:"))
+print(f"Hello User:{User_Name} your age is:{User_Age}")
