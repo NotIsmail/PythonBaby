ef142497293beacd34f803cc9a31c8d8ad24b220
NotIsmail <mohammedismail61405@gmail.com>
2024-01-27 15:18:22 +0530

    Python learnt

--- Hello.py
+++ Hello.py
@@ -6,6 +6,31 @@ Author_Likings, Author_Movies, Author_Games = "Gaming", "La la land", "GTA 5"
 print(
     f"Your Author {Author_Name} Likes {Author_Likings} {Author_Movies} {Author_Games}"
 )
-User_Name = input("Enter your Name:")
+
+
+User_Name = input("Enter your Name:").strip().capitalize().title()
 User_Age = int(input("Enter your Age:"))
+# A Method where we clear up the unecessary spaces and as well as capitalizing things
+
+
+# User_Name = User_Name.strip()
+
+
+# Now we see that wen we wrote the full name only the first word was capitalized so to fix these we use title which capitalize every word's first letter (the previous function is commented due to the problem as mentioned in the abpve comment )
+# User_Name=User_Name.capitalize()
+
+
+# User_Name = User_Name.title()
+
+# There are certain ways where we can reduce the number of lines so unlike the upper two methods (which is commented out) we can write it this way
+
+# User_Name=User_Name.strip().capitalize().title()
+
+#there is another way to even reduce the lines of code and that is mapping back to the input line and the previous method is commented out
+
 print(f"Hello User:{User_Name} your age is:{User_Age}")
+
+# we are gonna look at another method to access the first and last name
+
+first_name,last_name=User_Name.split(" ")
+print(f"Hello {first_name}")
---
+++ Int_Prac.py
@@ -0,0 +1,15 @@
+#now we aree looking at working with numbers and other functions which help us with various integer works
+Num_1=int(input("Enter Number 1:"))
+Num_2=int(input("Enter Number 2:"))
+res=Num_1+Num_2
+print(f"{Num_1} + {Num_2} = {res}")
+
+
+# now when we are using float numbers we can round it off 
+Flo_1=float(input("Enter the float number 1:"))
+Flo_2=float(input("Enter the float number 2:"))
+Flo_Res=(Flo_1/Flo_2)
+
+# now there are various conventions where we can separate the digits thru comma or dots or any other way
+
+print(f"{Flo_Res:.2f}")
