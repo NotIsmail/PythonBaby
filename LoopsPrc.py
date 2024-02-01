ac06e223e38cf434f9b6064a5c4dd16f3fd3e2af
NotIsmail <mohammedismail61405@gmail.com>
2024-02-01 23:02:00 +0530

    Studying Python

---
+++ LoopsPrc.py
@@ -0,0 +1,5 @@
+# Today we are going to to learn loop
+i = 3  # the condition can be i=1
+while i != 0:  # the condition can also be i<=3
+    print("Save me from the new changes please")
+    i = i - 1  #so for the above commented conditions it can be changed to i=i+1
