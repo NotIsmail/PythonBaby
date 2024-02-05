ab106ec4750d1998a79ce7471bf1cb74eef64ff7
NotIsmail <mohammedismail61405@gmail.com>
2024-02-05 22:49:54 +0530

    The modules of python

---
+++ Modulesandlibs.py
@@ -0,0 +1,7 @@
+# In this we will see the use of one of the various modules availabe
+
+
+import random  # the modules r introduced using impport keyword
+
+coin = random.choice(["Heads", "Tails"])
+print(coin)
