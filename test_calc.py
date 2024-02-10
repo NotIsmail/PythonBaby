84df70765f49d61de0fdcc090a1cb15dcf0a657e
NotIsmail <mohammedismail61405@gmail.com>
2024-02-10 23:43:17 +0530

    learnt using the package Pytest

--- test_calc.py
+++ test_calc.py
@@ -1,12 +1,13 @@
-from calcTest import calc_Sq
+from calc_Test import calc_Sq
 
 
 def main():
-    Imp_Sq()
+    test_Imp_Sq()
 
 
-def Imp_Sq():
+def test_Imp_Sq():
     # Now as we see there are horrendous error messages which  are difficult for user so let's put exceeptions
+    '''
     try:
         assert calc_Sq(2) == 4
     except AssertionError:
@@ -15,7 +16,15 @@ def Imp_Sq():
         assert calc_Sq(3) == 9
     except AssertionError:
         print("3 squared is not 9")
+    '''
 
+    # as in the above program we used various try and except to fix errors but it is not efficient as there is too many lines of code therefore to fix this
+
+    assert calc_Sq(2) == 4
+    assert calc_Sq(3) == 9
+
+
+# So as we see we need to remember a very important point of always naming the testing functions and files with the word "test_"/"_test" in them and remember to use lowercase t in "test".
 
 if __name__ == "__main__":
     main()
