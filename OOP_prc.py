9e5a355281edb4c17ed8decc47bf6ad1f7abaf39
NotIsmail <mohammedismail61405@gmail.com>
2024-02-21 21:01:48 +0530

    Learnt the use of init and self and none usage and functions

--- OOP_prc.py
+++ OOP_prc.py
@@ -1,15 +1,23 @@
-class Student: ...
+# we are noww seeing the use of classes which is useful for oop in python
+
+# now we r using the other method of using the classes
+
+
+class Student:
+    def __init__(self, name, profession) -> None:
+        self.name = name
+        self.profession = profession
 
 
 def main():
     student = Get_Name()
-    print(f"{student.name} profession:{student.profession}")
+    print(f"{student.name}  profession:{student.profession}")
 
 
 def Get_Name():
-    student = Student()
-    student.name = input("Enter the name:")
-    student.profession = input("Enter the profession:")
+    name = input("Enter the name:")
+    profession = input("Enter the profession:")
+    student = Student(name, profession)
     return student
 
 
