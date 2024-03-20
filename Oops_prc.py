20d3053e6274dab075e5c27b0b950fa289ea4fcb
NotIsmail <mohammedismail61405@gmail.com>
2024-03-20 21:39:25 +0530

    learnt creating objects and then using 'if' 'not' and 'raise' etc

--- Oops_prc.py
+++ Oops_prc.py
@@ -18,6 +18,9 @@ if __name__=="__main__":
     main()
 '''
 # Just another way to do it is it is not working to (T_T)
+
+
+'''
 class Student:
     ...
 
@@ -32,5 +35,38 @@ def get_student():                                          # this is called att
     Student=Student(name,genre,movie)
     return Student
 
+if __name__=="__main__":
+    main()
+
+'''
+
+
+# Another method 
+
+# so in this lecture we have leanrt to raise exceptions where a user might make some errors in filling in details so for exapmle ..
+# and another use of using the class is to give a detailed and complex if conditions for example a lost full of values which is true for the if block
+
+class Student:
+    def __init__(self,name,genre,movie) -> None:
+        if not name:
+            raise ValueError("Missing name")        # we r trying and setting up a condition where if name not entered = valueerror
+        if not genre in ["romance","drama","action","biographhy","war","crime","fantasy","sci-fi","thriller"]:
+            raise ValueError("wrong genre")
+        self.name=name
+        self.genre=genre
+        self.movie=movie
+
+def main():
+    stu_data=get_student()
+    print(f"{stu_data.name}'s favorite genre is {stu_data.genre} and is favorite movie is {stu_data.movie}")
+
+def get_student():                                          # this is called attributes 
+    name=input("Enter your name:")
+    genre=input("Enter your favorite movie genre:")
+    movie=input("Enter your favorite movie:")
+    try:                                                    #here we use the try and except to as usual to handle exceptions and error
+        return Student(name,genre,movie)
+    except:
+        print("Please enter your name")
 if __name__=="__main__":
     main()
