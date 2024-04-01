5e42f91a32f59c048b869d415f36a0a16ec08dce
NotIsmail <mohammedismail61405@gmail.com>
2024-04-01 20:05:43 +0530

    Learnt the use of __str__ method

--- Oops_prc.py
+++ Oops_prc.py
@@ -56,9 +56,14 @@ class Student:
         self.genre=genre
         self.movie=movie
 
+    def __str__(self):
+        '''return "a person"'''  #so this thing kind of returns a value wen no data is entered and not a cryptic memory location we got some issue the required output wasn't recieved so the below value works as much as the print statement but also returns in redable form not like in the print statement
+        return f"{self.name} likes {self.genre} likes {self.movie}"
+
+
 def main():
     stu_data=get_student()
-    print(f"{stu_data.name}'s favorite genre is {stu_data.genre} and is favorite movie is {stu_data.movie}")
+    print(stu_data)       # the code in this print statement kind of accessed the obj and then printed it but now to reduc the code we did this and when we run this code we get a cryptic output stating it's location in the computer's memory so to fix this we kinda use the above def __str__ method
 
 def get_student():                                          # this is called attributes 
     name=input("Enter your name:")
